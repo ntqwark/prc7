0d89d4aa3a755b8ad1543d105194dc8715a69e19
ntqwark <qwark4media@gmail.com>
2020-09-03 21:05:37 +0300

    <3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,97 @@
+// Main.java:
+
 package com.company;
 
-import java.io.*;
 import java.lang.String;
 
 public class Main
 {
     public static void main(String[] args) {
-        Circle[] t = new Circle[10];
+        Dish[] dishes = new Dish[10];
 
-        for (int i = 0; i < 10; i++) {
-            t[i] = new Circle();
+        for (int i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                var t = new Plate();
+                t.setSize((float) (Math.random() * 100),
+                        (float) (Math.random() * 100),
+                        (float) (Math.random() * 100));
 
-            t[i].setRadius(i + 1);
-            t[i].setColor("Color number " + i);
+                t.setModel("Модель 1A");
+                t.setRadius((float) (Math.random() * 100));
+
+                dishes[i] = t;
+            }
+            else
+            {
+                var t = new Cup();
+
+                t.setSize((float) (Math.random() * 100),
+                        (float) (Math.random() * 100),
+                        (float) (Math.random() * 100));
+
+                t.setColor("Orange");
+
+                dishes[i] = t;
+            }
         }
 
         for (int i = 0; i < 10; i++)
         {
-            System.out.println(t[i].toString());
+            System.out.println(dishes[i] + "\n");
         }
     }
 }
+
+/*
+ Output:
+
+Тарелочка. Параметры:
+Модель - Модель 1A
+Радиус - 61
+Размеры: (x, y, z) = (75, 84, 89)
+
+Кружечка. Параметры:
+Цвет - Orange
+Размеры: (x, y, z) = (5, 92, 82)
+
+Тарелочка. Параметры:
+Модель - Модель 1A
+Радиус - 21
+Размеры: (x, y, z) = (83, 77, 76)
+
+Кружечка. Параметры:
+Цвет - Orange
+Размеры: (x, y, z) = (97, 91, 9)
+
+Тарелочка. Параметры:
+Модель - Модель 1A
+Радиус - 17
+Размеры: (x, y, z) = (67, 41, 62)
+
+Кружечка. Параметры:
+Цвет - Orange
+Размеры: (x, y, z) = (68, 86, 78)
+
+Тарелочка. Параметры:
+Модель - Модель 1A
+Радиус - 7
+Размеры: (x, y, z) = (97, 14, 39)
+
+Кружечка. Параметры:
+Цвет - Orange
+Размеры: (x, y, z) = (2, 19, 6)
+
+Тарелочка. Параметры:
+Модель - Модель 1A
+Радиус - 96
+Размеры: (x, y, z) = (52, 85, 76)
+
+Кружечка. Параметры:
+Цвет - Orange
+Размеры: (x, y, z) = (4, 87, 39)
+
+
+Process finished with exit code 0
+ */
--- src/com/company/Models.java
+++ src/com/company/Models.java
@@ -1,32 +1,63 @@
+// Models.java:
+
+
 package com.company;
 
-class Circle
+class Cup extends Dish
 {
-    private int radius;
     private String color;
 
-    public void setRadius(int radius)
-    {
-        this.radius = radius;
-    }
-
-    public int getRadius()
-    {
-        return radius;
-    }
-
-    public void setColor(String color)
-    {
-        this.color = color ;
-    }
-
-    public String getColor()
-    {
+    public String getColor() {
         return color;
     }
 
-    public String toString()
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Кружечка. Параметры:\nЦвет - %s\nРазмеры: (x, y, z) = (%d, %d, %d)",
+                getColor(), (int)size_x, (int)size_y, (int)size_z);
+    }
+}
+
+class Plate extends Dish
+{
+    private String model;
+    private float radius;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Тарелочка. Параметры:\nМодель - %s\nРадиус - %d\nРазмеры: (x, y, z) = (%d, %d, %d)",
+                getModel(), (int)getRadius(), (int)size_x, (int)size_y, (int)size_z);
+    }
+}
+
+abstract class Dish
+{
+    protected float size_x, size_y, size_z;
+
+    public void setSize(float x, float y, float z)
     {
-        return String.format("Circle Info: radius=%d, color=%s", getRadius(), getColor());
+        size_x = x;
+        size_y = y;
+        size_z = z;
     }
 }
