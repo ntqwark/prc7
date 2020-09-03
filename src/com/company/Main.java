82bf6ceedaefd1b92cc83087d6718fbf3cb904bc
ntqwark <qwark4media@gmail.com>
2020-09-03 20:46:09 +0300

    hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,60 @@
+// Main.java
 
 package com.company;
 
-import java.io.*;
-import java.lang.String;
+import java.awt.*;
+import javax.swing.*;
 
-public class Main
-{
-    static double factorial(int t)
+
+class Main extends JFrame {
+    int milanScore  = 0;
+    int madridScore = 0;
+
+    JButton btn1 = new JButton("AC Milan");
+    JButton btn2 = new JButton("Real Madrid");
+
+    JLabel text1 = new JLabel("Result: 0 X 0");
+    JLabel text2 = new JLabel("Last Scorer: N/A");
+    Label  text3 = new Label("Winner: N/A");
+
+    void printResults()
     {
-        float result = 1;
+        text1.setText(String.format("Result: %d X %d", milanScore, madridScore));
 
-        for (int i = 1; i < t; i++)
-        {
-            result = result + result * i;
-        }
-
-        return result;
+        if (milanScore > madridScore) text3.setText("Winner: AC Milan");
+        else text3.setText("Winner: Real Madrid");
     }
 
-    public static void main(String[] args)
-    {
-        String input = "";
+    public Main() {
+        super("prc 4");
+        super.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        try {
-            System.out.print("Введите число: ");
-            java.io.BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            input = in.readLine();
-        } catch (Exception ignored) { }
+        setSize(300, 300);
+        setLayout(new FlowLayout());
 
-        System.out.print("Факториал: ");
-        System.out.println(factorial(Integer.parseInt(input)) + "\n");
-        main(args);
+        add(btn1);
+        add(btn2);
+
+        add(text1);
+        add(text2);
+        add(text3);
+
+        btn1.addActionListener(ae -> {
+            milanScore++;
+            text2.setText("Last Scorer: AC Milan");
+
+            printResults();
+        });
+
+        btn2.addActionListener(ae-> {
+            madridScore++;
+            text2.setText("Last Scorer: Real Madrid");
+
+            printResults();
+        });
+    }
+
+    public static void main(String[] args) {
+        new Main().setVisible(true);
     }
 }
