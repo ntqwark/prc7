bc96a369e26b49c5ba6afe22a461e31ed86885e2
ntqwark <qwark4media@gmail.com>
2020-09-03 21:12:03 +0300

    <3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,50 +1,155 @@
-// Main.java:
+// Main.java
 
 package com.company;
 
-import java.lang.String;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
-public class Main
+interface IDrawable
 {
-    public static void main(String[] args) {
-        Object[] objects = new Object[10];
+    Polygon getPolygone();
+}
 
-        for (int i = 0; i < 10; i++)
+class Quadrangle implements IDrawable
+{
+    int[] xCoords = new int[4];
+    int[] yCoords = new int[4];
+
+    public Quadrangle(Point[] points)
+    {
+        for (int i = 0; i < 4; i++)
         {
-            if (i % 3 == 0) {
-                objects[i] = new Object();
-            } else {
-                var t  = new Product();
-                t.setPrice(1000 * (i + 1));
-
-                objects[i] = t;
-            }
+            xCoords[i] = points[i].x;
+            yCoords[i] = points[i].y;
         }
+    }
 
-        for (int i = 0; i < 10; i++)
-        {
-            if (objects[i] instanceof IPriceable) {
-                System.out.println("cost = " + ((IPriceable)objects[i]).getPrice());
-            } else {
-                System.out.println("Объект не реализует интерфейс IPriceable");
-            }
-        }
+    public Quadrangle() { }
+
+    @Override
+    public Polygon getPolygone() {
+        return new Polygon(xCoords, yCoords, 4);
     }
 }
 
-/*
-Output:
+class Rect extends Quadrangle
+{
+    int[] xCoords = new int[4];
+    int[] yCoords = new int[4];
 
-Объект не реализует интерфейс IPriceable
-cost = 2000
-cost = 3000
-Объект не реализует интерфейс IPriceable
-cost = 5000
-cost = 6000
-Объект не реализует интерфейс IPriceable
-cost = 8000
-cost = 9000
-Объект не реализует интерфейс IPriceable
+    public Rect(int x1, int y1, int x2, int y2)
+    {
+        xCoords[0] = x1;
+        yCoords[0] = y1;
 
-Process finished with exit code 0
- */
+        xCoords[1] = x1;
+        yCoords[1] = y2;
+
+        xCoords[2] = x2;
+        yCoords[2] = y2;
+
+        xCoords[3] = x2;
+        yCoords[3] = y1;
+    }
+
+    @Override
+    public Polygon getPolygone() {
+        return new Polygon(xCoords, yCoords, 4);
+    }
+}
+
+public class Main {
+
+    private JFrame mainMap;
+
+    public Main() {
+
+        initComponents();
+
+    }
+
+    private void drawShape(Graphics graphics, Color color, IDrawable drawable)
+    {
+        var oldColor = graphics.getColor();
+        graphics.setColor(color);
+
+        graphics.fillPolygon((drawable.getPolygone()));
+
+        graphics.setColor(oldColor);
+    }
+
+    public int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+
+    private Color getRandomColor()
+    {
+        int color = getRandomNumber(1, 5);
+
+        switch (color)
+        {
+            case 1: return new Color(253, 79, 79);
+            case 2: return new Color(79, 253, 113);
+            case 3: return new Color(79, 166, 253);
+            case 4: return new Color(253, 195, 79);
+            case 5: return new Color(253, 79, 219);
+        }
+
+        return new Color(0, 255, 0);
+    }
+
+    private void drawShapes(Graphics graphics)
+    {
+        int row = 0, collumn = 0;
+        int size = 100;
+
+        for (int i = 1; i < 21; i++)
+        {
+            Point startPos = new Point(row++ * size + 20, collumn * size +  20);
+            var rect = new Rect(startPos.x, startPos.y, startPos.x + size - 20 , startPos.y + size - 20);
+
+            drawShape(graphics, getRandomColor(), rect);
+
+            if (i % 5 == 0)
+            {
+                collumn++;
+                row = 0;
+            }
+        }
+    }
+
+    private void initComponents() {
+
+        mainMap = new JFrame("Ivan T. / Lab5 / 11-19");
+        mainMap.setResizable(false);
+
+        mainMap.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        JPanel p = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+
+                drawShapes(g);
+            }
+
+            @Override
+            public Dimension getPreferredSize() {
+                return new Dimension(520, 420);
+            }
+        };
+
+        mainMap.add(p);
+        mainMap.pack();
+        mainMap.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Main());
+    }
+}
+
+// Result:
+// https://i.imgur.com/sQ5CkRA.png
