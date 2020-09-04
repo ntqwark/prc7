32ce9578a56f7f7a4a6ff4a6d8d0d9940fe39f03
ntqwark <qwark4media@gmail.com>
2020-09-04 18:47:44 +0300

    <3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,65 +2,22 @@
 
 package com.company;
 
-import java.awt.*;
-import javax.swing.*;
+import java.util.ArrayList;
 
-class Main extends JFrame {
-    int rand = (int) (Math.random() * 10);
-    int attemps = 3;
-
-    JTextField input = new JTextField(20);
-    JButton btn = new JButton("ok.");
-
-    void process()
+class Main
+{
+    public static void main(String[] args)
     {
-        try {
-            int t = Integer.parseInt(input.getText());
+        ArrayList<Integer> list = new ArrayList<>();
 
-            if (t > rand)
-                JOptionPane.showMessageDialog(
-                        null,
-                        String.format("Искомое число меньше.\nОставшиеся попытки: %d", attemps--),
-                        "Важная информация",
-                        JOptionPane.INFORMATION_MESSAGE);
+        for (int i = 0; i < 10; i++)
+        {
+            list.add(i);
+        }
 
-            else if (t < rand)
-                JOptionPane.showMessageDialog(
-                        null,
-                        String.format("Искомое число больше.\nОставшиеся попытки: %d", attemps--),
-                        "Важная информация",
-                        JOptionPane.INFORMATION_MESSAGE);
-
-            else {
-                JOptionPane.showMessageDialog(
-                        null,
-                        String.format("Вы выиграли!", attemps--),
-                        "Важная информация",
-                        JOptionPane.INFORMATION_MESSAGE);
-
-                System.exit(0);
-            }
-
-            if (attemps < 0) { System.exit(0); }
-        } catch (Exception ignored) { }
-    }
-
-    public Main() {
-        super("lab 6");
-        super.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-        setSize(300, 100);
-        setLayout(new FlowLayout());
-
-        add(input);
-        add(btn);
-
-        btn.addActionListener(t -> {
-            process();
-        });
-    }
-
-    public static void main(String[] args) {
-        new Main().setVisible(true);
+        for (var t : list)
+        {
+            System.out.println(t);
+        }
     }
 }
