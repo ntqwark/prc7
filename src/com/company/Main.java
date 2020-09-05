0e6157e5daad87abb600eb9a841cfacaa68e8197
ntqwark <qwark4media@gmail.com>
2020-09-05 18:36:59 +0300

    <3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -52,3 +52,42 @@ class Main
         for (var t : students) System.out.println(t);
     }
 }
+
+// Output:
+/*
+Student{ id=1, firstname='Noah', secondname='Adderiy', age=16 }
+Student{ id=2, firstname='Liam', secondname='Allford', age=17 }
+Student{ id=3, firstname='William', secondname='Arnold', age=18 }
+Student{ id=4, firstname='Mason', secondname='Adderiy', age=19 }
+Student{ id=5, firstname='James', secondname='Allford', age=20 }
+Student{ id=6, firstname='Benjamin', secondname='Arnold', age=21 }
+Student{ id=7, firstname='Noah', secondname='Adderiy', age=22 }
+Student{ id=8, firstname='Liam', secondname='Allford', age=23 }
+Student{ id=9, firstname='William', secondname='Arnold', age=24 }
+Student{ id=10, firstname='Mason', secondname='Adderiy', age=25 }
+
+Student{ id=10, firstname='Mason', secondname='Adderiy', age=25 }
+Student{ id=9, firstname='William', secondname='Arnold', age=24 }
+Student{ id=8, firstname='Liam', secondname='Allford', age=23 }
+Student{ id=7, firstname='Noah', secondname='Adderiy', age=22 }
+Student{ id=6, firstname='Benjamin', secondname='Arnold', age=21 }
+Student{ id=5, firstname='James', secondname='Allford', age=20 }
+Student{ id=4, firstname='Mason', secondname='Adderiy', age=19 }
+Student{ id=3, firstname='William', secondname='Arnold', age=18 }
+Student{ id=2, firstname='Liam', secondname='Allford', age=17 }
+Student{ id=1, firstname='Noah', secondname='Adderiy', age=16 }
+
+Student{ id=1, firstname='Noah', secondname='Adderiy', age=16 }
+Student{ id=2, firstname='Liam', secondname='Allford', age=17 }
+Student{ id=3, firstname='William', secondname='Arnold', age=18 }
+Student{ id=4, firstname='Mason', secondname='Adderiy', age=19 }
+Student{ id=5, firstname='James', secondname='Allford', age=20 }
+Student{ id=6, firstname='Benjamin', secondname='Arnold', age=21 }
+Student{ id=7, firstname='Noah', secondname='Adderiy', age=22 }
+Student{ id=8, firstname='Liam', secondname='Allford', age=23 }
+Student{ id=9, firstname='William', secondname='Arnold', age=24 }
+Student{ id=10, firstname='Mason', secondname='Adderiy', age=25 }
+
+Process finished with exit code 0
+
+ */
